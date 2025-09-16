86735703bd8f734d0db6dd98cee2989241455e94
hypoxisaurea <flickerlight14@naver.com>
2025-09-16 13:17:45 +0900

    [ADD] Media Function

--- chrome-extension/src/pages/StartPage.tsx
+++ chrome-extension/src/pages/StartPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from 'components/common/Logo';
-import Slogan from 'components/LandingPage/Slogan';
+import Slogan from 'components/StartPage/Slogan';
 import Button from 'components/common/BlackButton';
 import { HorizontalSpacing } from 'components/common';
 
