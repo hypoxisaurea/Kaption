2311a50117eeecc0bfa30e72f3091b7e3ca3fedf
hypoxisaurea <flickerlight14@naver.com>
2025-09-16 17:07:22 +0900

    [Styles] Redesign Header

--- chrome-extension/src/components/index.ts
+++ chrome-extension/src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Header } from './common/Header';
+export { default as Header } from './common/ColorHeader';
 export { default as Logo } from './common/Logo';
 export { default as Footer } from './common/Footer';
 export { default as BlackButton } from './common/BlackButton';
