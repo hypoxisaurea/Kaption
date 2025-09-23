5bfa6be6b3cd71203ede16ccbb287bb8a5d511e5
hypoxisaurea <flickerlight14@naver.com>
2025-09-23 15:43:55 +0900

    [Update] Landing, Start Page

--- chrome-extension/src/pages/StartPage.tsx
+++ chrome-extension/src/pages/StartPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Logo from 'components/common/Logo';
+import Logo from 'assets/images/logo/logo_text_shadow.png';
+import Taki from 'assets/images/character/taki.png';
 import Slogan from 'components/StartPage/Slogan';
 import Button from 'components/common/BlackButton';
 import { HorizontalSpacing } from 'components/common';
@@ -23,8 +24,10 @@ function StartPage() {
       className={`flex h-screen flex-col items-center justify-center overflow-hidden bg-white ${isVisible ? 'opacity-100' : 'opacity-0'} transition-opacity duration-[350ms]`}
       style={{ willChange: 'opacity' }}
     >
-      <Logo width='17%'/>
-      <HorizontalSpacing height='12%'/>
+      <img src={Taki} alt='Taki' width='60%'/>
+      <HorizontalSpacing height='10%'/>
+      <img src={Logo} alt='Logo' width='40%'/>
+      <HorizontalSpacing height='5%'/>
       <Slogan>Kaption uncovers the hidden cultural gems in K-content</Slogan>
       <HorizontalSpacing height='0.5%'/>
       <Slogan>Dive deeper into Korea</Slogan>
@@ -32,7 +35,7 @@ function StartPage() {
       <Slogan>Faster, Smarter, Better - </Slogan>
       <HorizontalSpacing height='4%'/>
       <div className='w-full max-w-[50%] px-8'>
-        <Button fullWidth bgColor="bg-black" textColor="text-white" className="text-sm" onClick={handleButtonClick}>Get Started</Button>
+        <Button fullWidth bgColor="bg-black" textColor="text-white" className="text-[3vw]" onClick={handleButtonClick}>Get Started</Button>
       </div>
     </div>
   )
