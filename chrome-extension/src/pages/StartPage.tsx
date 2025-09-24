a6a7c789885702c5efab391736f5006ad4fe919d
hypoxisaurea <flickerlight14@naver.com>
2025-09-24 12:03:08 +0900

    [UPDATE] Refactoring

--- chrome-extension/src/pages/StartPage.tsx
+++ chrome-extension/src/pages/StartPage.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from 'assets/images/logo/logo_text_shadow.png';
 import Taki from 'assets/images/character/taki.png';
-import Slogan from 'components/StartPage/Slogan';
-import Button from 'components/common/BlackButton';
-import { HorizontalSpacing } from 'components/common';
+import { Slogan, BlackButton as Button, HorizontalSpacing } from 'components';
+import useFadeIn from 'hooks/useFadeIn';
 
 function StartPage() {
   const navigate = useNavigate();
@@ -12,19 +11,14 @@ function StartPage() {
     navigate('/option');
   };
 
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const raf = requestAnimationFrame(() => setIsVisible(true));
-    return () => cancelAnimationFrame(raf);
-  }, []);
+  const isVisible = useFadeIn();
 
   return (
     <div
       className={`flex h-screen flex-col items-center justify-center overflow-hidden bg-white ${isVisible ? 'opacity-100' : 'opacity-0'} transition-opacity duration-[350ms]`}
       style={{ willChange: 'opacity' }}
     >
-      <img src={Taki} alt='Taki' width='60%'/>
+      <img src={Taki} alt='Taki' width='80%'/>
       <HorizontalSpacing height='10%'/>
       <img src={Logo} alt='Logo' width='40%'/>
       <HorizontalSpacing height='5%'/>
