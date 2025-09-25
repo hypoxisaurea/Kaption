e32912de96ad46f362bf8631fef97b9188985743
hypoxisaurea <flickerlight14@naver.com>
2025-09-25 13:24:49 +0900

    [UPDATE] refactoring

--- chrome-extension/src/components/common/WhiteHeader.tsx
+++ chrome-extension/src/components/common/WhiteHeader.tsx
@@ -7,9 +7,9 @@ import Menu from 'assets/images/icon/button_menu_white.png';
 
 function WhiteHeader() {
     return (
-        <div className="box-border flex w-full items-center justify-between px-10 py-8">
+        <div className="box-border bg-[#1b1b1b] flex w-full items-center justify-between px-10 py-8">
             <div className="flex shrink-0 grow-0 items-center justify-start">
-                <Link to='/' className='block'>
+                <Link to='/option' className='block'>
                     <img src={Logo} alt='Logo' className='block h-auto w-[6vw] shrink-0' />
                 </Link>
             </div>
